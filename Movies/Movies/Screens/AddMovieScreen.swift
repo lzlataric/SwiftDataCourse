9123ca14c84457e60a8471ccc5be7bfb0513244f
Luka Zlatarić <luka.zlataric2402@gmail.com>
2025-01-28 08:13:19 +0100

    Showing Movies list

--- Movies/Movies/Screens/AddMovieScreen.swift
+++ Movies/Movies/Screens/AddMovieScreen.swift
@@ -23,6 +23,7 @@ struct AddMovieScreen: View {
             TextField("Title", text: $title)
             TextField("Year", value: $year, format: .number)
         }
+        .navigationTitle("Add New Movie")
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 Button("Close") {
@@ -40,6 +41,7 @@ struct AddMovieScreen: View {
                     } catch {
                         print(error.localizedDescription)
                     }
+                    dismiss()
                 }
                 .disabled(!isFormValid)
             }
---
+++ Movies/Movies/Screens/MovieListScreen.swift
@@ -0,0 +1,39 @@
+//
+//  MovieListScreen.swift
+//  Movies
+//
+//  Created by Luka Zlataric on 28.01.2025..
+//
+
+import SwiftUI
+import SwiftData
+
+struct MovieListScreen: View {
+    @Query(sort: \Movie.title, order: .forward) private var movies: [Movie]
+    @State private var isAddMoviePresented: Bool = false
+    
+    var body: some View {
+        List(movies) { movie in
+            Text(movie.title)
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Add") {
+                    isAddMoviePresented = true
+                }
+            }
+        }
+        .sheet(isPresented: $isAddMoviePresented) {
+            NavigationStack {
+                AddMovieScreen()
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        MovieListScreen()
+            .modelContainer(for: [Movie.self])
+    }
+}
