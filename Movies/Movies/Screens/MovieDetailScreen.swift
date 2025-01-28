e66df9ccbf9601c638675b750ba5bc1130d0c0ef
Luka Zlatarić <luka.zlataric2402@gmail.com>
2025-01-28 08:42:15 +0100

    Edit movie

---
+++ Movies/Movies/Screens/MovieDetailScreen.swift
@@ -0,0 +1,65 @@
+//
+//  MovieDetailScreen.swift
+//  Movies
+//
+//  Created by Luka Zlataric on 28.01.2025..
+//
+
+import SwiftUI
+import SwiftData
+
+struct MovieDetailScreen: View {
+    @Environment(\.modelContext) private var context
+    @Environment(\.dismiss) private var dismiss
+    let movie: Movie
+    @State private var title: String = ""
+    @State private var year: Int?
+    
+    var body: some View {
+        Form {
+            TextField("Title", text: $title)
+            TextField("Year", value: $year, format: .number)
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Edit") {
+                    guard let year = year else { return }
+                    movie.title = title
+                    movie.year = year
+                    do {
+                        try context.save()
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                    dismiss()
+                }
+            }
+        }
+        .onAppear {
+            title = movie.title
+            year = movie.year
+        }
+    }
+}
+
+//For Previews
+struct MovieDetailContainerScreen: View {
+    @Environment(\.modelContext) private var context
+    @State private var movie: Movie?
+    var body: some View {
+        ZStack {
+            if let movie {
+                MovieDetailScreen(movie: movie)
+            }
+        }
+        .onAppear {
+            let movie = Movie(title: "Shutter Island", year: 2009)
+            context.insert(movie)
+        }
+    }
+}
+
+#Preview {
+    MovieDetailContainerScreen()
+        .modelContainer(for: [Movie.self])
+}
