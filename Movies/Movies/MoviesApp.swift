ca70dc9c7a1147a6784b2dfd22127a82315eec81
Luka Zlatarić <luka.zlataric2402@gmail.com>
2025-01-28 07:28:56 +0100

    Initial commit

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Luka Zlataric on 28.01.2025..
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
